13ec9777851a8d1f3ff35a9e45645d2e8432c154
Joao <jlolis1@gmail.com>
2019-12-17 16:14:19 -0300

    Ajustes de alinhamento no layout da home

--- wp-content/themes/tema_portifolio/home.php
+++ wp-content/themes/tema_portifolio/home.php
@@ -3,26 +3,28 @@
 
 <div class="container">
 
-<div class="home-perfil">
-    <img id="foto-perfil" src="https://educar.gt/wp-content/uploads/2018/05/foto-de-perfil-adecuada.jpg">
+    <div class="home-perfil">
+        <img id="foto-perfil" src="https://educar.gt/wp-content/uploads/2018/05/foto-de-perfil-adecuada.jpg">
 
 
-    <div class="logo-instagram">
-        <a href="https://www.instagram.com/"><img id="logo-instagram" src="https://i.pinimg.com/originals/a7/2b/b5/a72bb5bf31303bd5f2fcf20c95603150.png"></a>
-    </div>
+        <div class="logo-instagram">
+            <a href="https://www.instagram.com/"><img id="logo-instagram" src="https://i.pinimg.com/originals/a7/2b/b5/a72bb5bf31303bd5f2fcf20c95603150.png"></a>
+        </div>
+                
             
-        
-    <ul id="lista-perfil">
-        <li id="lista-nome">Amanda Magri de Abreu</li>
-        <li id="lista-idade">25 Anos</li>
-        <li id="lista-endereço">São Carlos, SP - Brasil</li>
-        <li id="lista-resumo">Tradutora e Legendista, dos idiomas Inglês, Português e Francês</li>
-    </ul>
+        <ul id="lista-perfil">
+            <li id="lista-nome">Amanda Magri de Abreu</li>
+            <li id="lista-idade">25 Anos</li>
+            <li id="lista-endereço">São Carlos, SP - Brasil</li>
+            <li id="lista-resumo">Tradutora e Legendista, dos idiomas Inglês, Português e Francês</li>
+        </ul>
 
-</div>
-<div id="post-recente" class="post-recente">
-    <h2 class="post-recente" id="post-recente-texto">Ultima Postagem</h2>
-    <a href="https://www.google.com/" target="blank"><img id="post-recente-foto" src="https://www.tamiresdecarvalho.com/wp-content/uploads/2019/03/a-mulher-de-branco.jpg"></a>
+    </div>
+    <div id="post-recente" class="post-recente">
+        <h2 class="post-recente" id="post-recente-texto">Ultima Postagem</h2>
+        <a href="https://www.google.com/" target="blank"><img id="post-recente-foto" src="https://www.tamiresdecarvalho.com/wp-content/uploads/2019/03/a-mulher-de-branco.jpg"></a>
+    </div>
+    
 </div>
 
 <div id="citacao">
@@ -32,5 +34,5 @@
 
 </div>
 
-<div class="container">
+
 
