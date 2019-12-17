41390a996dd810c3c8c0a063ca35fcbfcd05f2af
Joao <jlolis1@gmail.com>
2019-12-17 16:04:51 -0300

    Finalizado o Layout da Home

--- wp-content/themes/tema_portifolio/header.php
+++ wp-content/themes/tema_portifolio/header.php
@@ -14,17 +14,17 @@
     </head>
 
     <header>
-        <div class="cabecalho">
+        <div id="cabecalho" class="cabecalho">
 
             <div class="menu-mobile"><?php include 'img/menu-mobile.svg';?></div>
-            <ul class="lista-menu" style="width: 100%;">
-                <li>Home</li>
-                <li>Trabalhos</li>
-                <li>Vida de Tradutor</li>
-                <li>Contato</li>      
+
+            <ul class="lista-menu" id="lista-menu" style="width: 100%;">
+                <li><a href="home.php">Home</a></li>
+                <li><a href="home.php">Trabalhos</a></li>
+                <li><a href="home.php">Vida de Tradutor</a></li>
+                <li><a href="home.php">Contato</a></li>      
             </ul>
-        
-            
+             
         </div>
 
     <body>
